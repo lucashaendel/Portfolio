6ef9f6e13edb24a751f20dee32b98a2cb7a97dad
lucas h <lucas.ph@live.com>
2023-08-28 21:13:34 -0300

    bug fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -154,7 +154,6 @@ const Skills = () => {
             />
             <p>Postgres SQL</p>
           </figure>
-
           <figure class="icon-skill">
             <img
               src="https://img.icons8.com/fluency/480/000000/mysql-logo.png"
